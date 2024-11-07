7c5b2f1639be59fcfde40b7b1f7cadfe3a45c738
Tristan Tan <tristantan0899@gmail.com>
2024-11-08 04:08:32 +0800

    Commit 23

--- client/config.ts
+++ client/config.ts
@@ -6,7 +6,7 @@ const checkConfig = (server: string): Config | {} => {
   switch (server) {
     case "production":
       config = {
-        baseUrl: "http://localhost:8000",
+        baseUrl: "https://headphone-e-commerce2024server.vercel.app",
       };
       break;
     case "local":
